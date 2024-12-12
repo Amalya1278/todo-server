fced381d533e34474909ab62f733d72ce3e3b02e
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-12-12 23:20:17 +0400

    Delete todo directory

--- todo/src/components/todo-filter.jsx
+++
@@ -1,9 +0,0 @@
-export const TodoFilter = ({currentFilter,onSet}) => {
-  const list = ["all", "completed", "active"];
-
-  return (
-    <div className="flex justify-center items-center space-x-4">
-     
-    </div>
-  );
-};
